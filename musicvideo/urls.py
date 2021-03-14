af498c06e29d35ddc67b67ecb062f8ea21e585e4
Aman Gupta <60513655+aman-cpu@users.noreply.github.com>
2021-03-14 21:53:55 +0530

    Update urls.py

--- musicvideo/urls.py
+++ musicvideo/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .import category, subcategory, song, Admin, User
-from django.conf.urls import url
-from django.views.static import serve
-from django.conf import settings
+# from django.conf.urls import url
+# # from django.views.static import serve
+# # from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -62,7 +62,7 @@ urlpatterns = [
     path('searchsongjson/', User.ActionSearchSongJson),
     path('playsong/', User.ActionPlaySong),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 urlpatterns += staticfiles_urlpatterns()
