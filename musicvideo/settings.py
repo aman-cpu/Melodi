35cbdc399e70b8331cb521a9bb55ba0ebd34f8e9
Aman Gupta <60513655+aman-cpu@users.noreply.github.com>
2021-05-09 17:58:32 +0530

    Update settings.py

--- musicvideo/settings.py
+++ musicvideo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'oa-7so)1-yg@sox!n!nac_(rmz)hcb17ziws7mu*^!%ve1v7bn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['melodimusicapp.herokuapp.com']
 
