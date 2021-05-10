662d2982fb3fa451428279eecb70d99e1f1d552e
Aman Gupta <60513655+aman-cpu@users.noreply.github.com>
2021-05-10 11:13:29 +0530

    Update settings.py

--- musicvideo/settings.py
+++ musicvideo/settings.py
@@ -122,7 +122,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.normpath(os.path.join(BASE_DIR, 'staticfiles')))
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     Path.joinpath(BASE_DIR,'asset'),
