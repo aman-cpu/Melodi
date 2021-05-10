bd951d643d3add6645c34b1b8a1325eea401113e
Aman Gupta <60513655+aman-cpu@users.noreply.github.com>
2021-05-10 09:41:57 +0530

    Update settings.py

--- musicvideo/settings.py
+++ musicvideo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -128,4 +128,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     Path.joinpath(BASE_DIR,'asset'),
 )
-django_heroku.settings(locals())
+
